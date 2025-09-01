4576e79a1b4bf8ab707b1bf9b38839977be86244
manishr9919 <manishr9919@gamil.com>
2025-09-01 09:15:23 +0530

    01

---
+++ Linked_list/01.js
@@ -0,0 +1,19 @@
+ // add the Element 10,20,30 and print
+class node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+const head=new node(10)
+const second=new node(20)
+const third=new node(30)
+head.next=second
+second.next=third
+
+let current =head
+while(current){
+    console.log(current.data)
+    current=current.next
+}
+
